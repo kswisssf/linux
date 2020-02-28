d6e2fa75a30f9525559c80862ad9e2d3242daf72
majg <majg@fullhan.com>
2020-02-28 10:19:25 +0800

    modify signal

--- IPC/signal/signal.c
+++ IPC/signal/signal.c
@@ -7,8 +7,6 @@
 
 #include <signal.h>
 
-
-
 //  SIGINT ---- CTRL+C
 //  SIGQUIT ---- kill pid
 
@@ -22,18 +20,17 @@ int myfunc(int signo)
 	return 0;
 }
 
-// ps aux look for the pid
-//ps如何通过进程名来查找进程号
-//执行　ps -e | grep 进程名
-
 /*
+ps aux look for the pid
+ps如何通过进程名来查找进程号
+执行　ps -e | grep 进程名
+
 只查看该进程：ps -ef | grep pid
 查看该进程打开的文件：lsof -p pid
 查看内存分配：lcat /proc/pid/maps
 查看堆栈：pstack pid
 查看发出的系统调用:strace -p pid
 查看调用库函数:ltrace -p pid
-
 */
 
 int main(int argc, char *argv[])
@@ -45,5 +42,4 @@ int main(int argc, char *argv[])
 
 	pause();
 	exit(0);
-
 }
